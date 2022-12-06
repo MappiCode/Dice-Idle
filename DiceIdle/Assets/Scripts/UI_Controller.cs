8119ce98d0588a15921eb85ab691568c34a9183e
MappiCode <morten.napiralla@gmx.de>
2022-12-06 21:22:56 +0100

    Points to readable String #332ckck

--- DiceIdle/Assets/Scripts/UI_Controller.cs
+++ DiceIdle/Assets/Scripts/UI_Controller.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using TMPro;
 using Unity.VisualScripting;
+using UnityEngine.UIElements;
 
 public class UI_Controller : MonoBehaviour
 {
@@ -30,6 +32,42 @@ public class UI_Controller : MonoBehaviour
 
     public static void UpdatePoints()
     {
-        pointsValueTMP.text = Points_Manager.points.ToString();
+        double points = Points_Manager.points;
+        int powerOf10 = (int) Math.Floor(Math.Log10(points));
+        string pointsText = points.ToString();
+
+        if(powerOf10 > 2)
+            switch(powerOf10 % 3)
+            {
+                case 0:
+                    pointsText = pointsText.Substring(0, 1) + ',' + pointsText.Substring(1, 3);
+                    break;
+                case 1:
+                    pointsText = pointsText.Substring(0, 2) + ',' + pointsText.Substring(2, 3);
+                    break;
+                case 2:
+                    pointsText = pointsText.Substring(0, 3) + ',' + pointsText.Substring(3, 3);
+                    break;
+            }
+
+        switch (powerOf10)
+        {
+            case var expression when powerOf10 < 3:
+                break;
+            case var expression when powerOf10 < 6:
+                pointsText += " thousand";
+                break;
+            case var expression when powerOf10 < 9:
+                pointsText += " million";
+                break;
+            case var expression when powerOf10 < 12:
+                pointsText += " billion";
+                break;
+            case var expression when powerOf10 < 15:
+                pointsText += " trillion";
+                break;
+        }
+
+        pointsValueTMP.text = pointsText;
     }
 }
